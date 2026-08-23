8a8bb93a9bd6e39151c634bc71a583892929e718
NikhilSandavekar <55824655+NikhilSandavekar@users.noreply.github.com>
2026-08-23 15:54:44 +0530

    Refresh offline cache after interface label update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='azure-databricks-map-v4';
+const CACHE='azure-databricks-map-v5';
 const ROOT=new URL('./',self.registration.scope).href;
 const FILES=['./','./index.html','./azure-ecosystem-offline.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./og.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)));self.skipWaiting();});
